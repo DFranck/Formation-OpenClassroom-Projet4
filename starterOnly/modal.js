8a2325dbce0d1ca7b239c72da099bade126ee11d
DFranck <franckdufournet@hotmail.fr>
2023-07-19 15:52:01 +0700

    ajout de commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,29 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const FormCloser = document.querySelector(".close");
+//selection de tout les input de type text et checkbox
+const inputs = document.querySelectorAll(
+  "input.text-control, input.checkbox-input"
+);
+//Selection de toute les radio
+const whatChecked = Array.from(
+  document.querySelectorAll(
+    "#location1, #location2, #location3, #location4, #location5, #location6"
+  )
+);
+//selection des spans pour les messages derreurs
+const firstErrorSpan = document.querySelector(".first-error");
+const lastErrorSpan = document.querySelector(".last-error");
+const emailErrorSpan = document.querySelector(".email-error");
+const birthdateErrorSpan = document.querySelector(".birthdate-error");
+const quantityErrorSpan = document.querySelector(".quantity-error");
+const locationsErrorSpan = document.querySelector(".locations-error");
+const cgvErrorSpan = document.querySelector(".cgv-error");
+const infosEventsSpan = document.querySelector(".infos-events");
+const cgv = document.getElementById("checkbox1");
+const infosEvents = document.getElementById("checkbox2");
+// variable qui dois finir sur true pour valider le formulaire
+let formValid = true;
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -27,121 +50,42 @@ FormCloser.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
 
-//============================================================
-//with gpt4
-//============================================================
-// const formDataInput = document.querySelectorAll(".formData input");
-// const radioGroup = document.querySelectorAll(
-//   "input[type=radio][name=location]"
-// );
-// // Le formulaire doit être valide quand l'utilisateur clique sur "Submit"
-// document.querySelector("form").addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   // Conserver les données du formulaire (ne pas effacer le formulaire) lorsqu'il ne passe pas la validation.
-
-//   let allConditionsMet = true;
-
-//   // Les données doivent être saisies correctement :
-//   formDataInput.forEach((input) => {
-//     let inputValue = input.value;
-//     let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-//     let numberRegex = /^\d+(\.\d+)?$/;
-//     let isSelect = Array.from(radioGroup).some((radio) => radio.checked);
-//     let cvg = document.getElementById("checkbox1").checked;
-
-//     // (1) Le champ Prénom a un minimum de 2 caractères / n'est pas vide.
-//     if (input.id === "first" && inputValue.length <= 2) {
-//       console.log("Encore quelque caracteres! pour le prénom");
-//       allConditionsMet = false;
-//     }
-//     // (2) Le champ du nom de famille a un minimum de 2 caractères / n'est pas vide.
-//     if (input.id === "last" && inputValue.length <= 2) {
-//       console.log("Encore quelque caracteres! pour le nom");
-//       allConditionsMet = false;
-//     }
-//     // (3) L'adresse électronique est valide.
-//     if (input.id === "email" && !emailRegex.test(inputValue)) {
-//       console.log("adresse email non valide!");
-//       allConditionsMet = false;
-//     }
-//     // (4) Pour le nombre de concours, une valeur numérique est saisie.
-//     if (input.id === "quantity" && !numberRegex.test(inputValue)) {
-//       console.log("Attention, veuillez saisir un nombre entre 0 et 99");
-//       allConditionsMet = false;
-//     }
-//     // (5) Un bouton radio est sélectionné.
-//     if (!isSelect) {
-//       console.log("Veuillez selectionner une localisation");
-//       allConditionsMet = false;
-//     }
-//     // (6) La case des conditions générales est cochée, l'autre case est facultative / peut être laissée décochée.
-//     if (!cvg) {
-//       console.log("veuillez accepter les CGV");
-//       allConditionsMet = false;
-//     }
-//   });
-//   if (allConditionsMet) {
-//     console.log("formulaire validé!");
-//   } else {
-//     e.preventDefault();
-//   }
-// });
-
-//=======================================================
-// Retry alone 4e essai
-//=======================================================
-const inputs = document.querySelectorAll(
-  "input.text-control, input.checkbox-input"
-);
-const whatChecked = Array.from(
-  document.querySelectorAll(
-    "#location1, #location2, #location3, #location4, #location5, #location6"
-  )
-);
-const firstErrorSpan = document.querySelector(".first-error");
-const lastErrorSpan = document.querySelector(".last-error");
-const emailErrorSpan = document.querySelector(".email-error");
-const birthdateErrorSpan = document.querySelector(".birthdate-error");
-const quantityErrorSpan = document.querySelector(".quantity-error");
-const locationsErrorSpan = document.querySelector(".locations-error");
-const cgvErrorSpan = document.querySelector(".cgv-error");
-const infosEventsSpan = document.querySelector(".infos-events");
-const cgv = document.getElementById("checkbox1");
-const infosEvents = document.getElementById("checkbox2");
-
-let formValid = true;
-//APPEL FONCTION CHAQUE INPUT
+//Ecoute CHAQUE INPUT
 inputs.forEach((input) => {
   input.addEventListener("input", (e) => {
     checkinput(e.target);
   });
 });
-//FONCTION CONTROLE CHAQUE INPUT
+//FONCTION CONTROLE CHAQUE INPUT a chaque changement d'input
 const checkinput = (input) => {
   let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   let birthdateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
   let numberRegex = /^[0-9][0-9]?$/;
-
+  //controle if a radio is checked
   let isOneChecked = whatChecked.some((checkbox) => checkbox.checked);
-  console.log(`Dans ${input.id}, il est écrit ${input.value}`);
+  //controle le prénom et ajoute un message si celui ci n'est pas correct
   if (input.id === "first" && input.value.length < 3) {
     formValid = false;
     firstErrorSpan.innerHTML = `<p>Veuillez renseigner au minimum 3 caractères dans prénom</p>`;
   } else if (input.id === "first" && input.value.length >= 3) {
     firstErrorSpan.innerHTML = ``;
   }
+
+  //controle le nom et ajoute un message si celui ci n'est pas correct
   if (input.id === "last" && input.value.length < 3) {
     formValid = false;
     lastErrorSpan.innerHTML = `<p>Veuillez renseigner au minimum 3 caractères dans nom</p>`;
   } else if (input.id === "last" && input.value.length >= 3) {
     lastErrorSpan.innerHTML = ``;
   }
+  //controle lemail et ajoute un message si celui ci n'est pas correct
   if (input.id === "email" && !emailRegex.test(input.value)) {
     formValid = false;
     emailErrorSpan.innerHTML = `<p>Attention votre adresse email n'est pas valide</p>`;
   } else if (input.id === "email" && emailRegex.test(input.value)) {
     emailErrorSpan.innerHTML = ``;
   }
+  //controle la date de naissance et ajoute un message si celle ci n'est pas correct
   if (input.id === "birthdate" && !birthdateRegex.test(input.value)) {
     formValid = false;
     birthdateErrorSpan.innerHTML =
@@ -149,6 +93,7 @@ const checkinput = (input) => {
   } else if (input.id === "birthdate" && birthdateRegex.test(input.value)) {
     birthdateErrorSpan.innerHTML = "";
   }
+  //controle le nombre de tournoi et ajoute un message si celui ci n'est pas correct
   if (input.id === "quantity" && !numberRegex.test(input.value)) {
     formValid = false;
     quantityErrorSpan.innerHTML =
@@ -156,6 +101,7 @@ const checkinput = (input) => {
   } else if (input.id === "quantity" && numberRegex.test(input.value)) {
     quantityErrorSpan.innerHTML = "";
   }
+  //controle la selection de radio et ajoute un message si celle ci n'est pas correct
   if (!isOneChecked) {
     formValid = false;
     locationsErrorSpan.innerHTML =
@@ -163,12 +109,14 @@ const checkinput = (input) => {
   } else {
     locationsErrorSpan.innerHTML = "";
   }
+  //controle laccord des CGV et ajoute un message si celle ci n'est pas correct
   if (!cgv.checked) {
     formValid = false;
     cgvErrorSpan.innerHTML = "<p>Veuillez accepter les CGV</p>";
   } else {
     cgvErrorSpan.innerHTML = "";
   }
+  //controle si l'utilisateur souhaite recevoir des news et ajoute un message si celle ci est souhaitée
   if (infosEvents.checked) {
     infosEventsSpan.innerHTML =
       "<p>Nous vous tiendrons informé des prochains évèneemnts</p>";
