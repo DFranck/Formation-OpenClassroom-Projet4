68269521f2e6e0400e2cd2e0951a3ad1fce224ba
DFranck <franckdufournet@hotmail.fr>
2023-08-07 16:52:45 +0700

    isVariables + tri varaible/functions/events

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -6,7 +6,7 @@ function editNav() {
     x.className = "topnav";
   }
 }
-
+//Variables
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBtnCloser = document.querySelector(".close");
@@ -15,14 +15,6 @@ const birthdateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
 const numberRegex = /^[0-9][0-9]?$/;
 const cgv = document.getElementById("checkbox1");
 const infosEvents = document.getElementById("checkbox2");
-let formValid = false;
-let firstNameOk = false;
-let lastNameOk = false;
-let emailOk = false;
-let birthdateOk = false;
-let tournamentQuantityOk = false;
-let tournamentSelectOk = false;
-let cgvOk = false;
 const inputs = document.querySelectorAll(
   "input.text-control, input.checkbox-input"
 );
@@ -39,7 +31,95 @@ const quantityErrorSpan = document.querySelector(".quantity-error");
 const locationsErrorSpan = document.querySelector(".locations-error");
 const cgvErrorSpan = document.querySelector(".cgv-error");
 const infosEventsSpan = document.querySelector(".infos-events");
+let isFormValid = false;
+let isFirstNameOk = false;
+let isLastNameOk = false;
+let isEmailOk = false;
+let isBirthdateOk = false;
+let isTournamentQuantityOk = false;
+let isTournamentSelectOk = false;
+let isCgvOk = false;
 
+//Functions
+const inputControl = (input) => {
+  if (input.id === "firstName") {
+    isFirstNameOk = input.value.replaceAll(" ", "").length >= 3;
+    firstNameErrorSpan.style.opacity = isFirstNameOk ? 0 : 1;
+  }
+  if (input.id === "lastName") {
+    isLastNameOk = input.value.replaceAll(" ", "").length >= 3;
+    lastNameErrorSpan.style.opacity = isLastNameOk ? 0 : 1;
+  }
+  if (input.id === "email") {
+    isEmailOk = emailRegex.test(input.value);
+    emailErrorSpan.style.opacity = isEmailOk ? 0 : 1;
+  }
+  if (input.id === "birthdate") {
+    isBirthdateOk = birthdateRegex.test(input.value);
+    birthdateErrorSpan.style.opacity = isBirthdateOk ? 0 : 1;
+  }
+  if (input.id === "quantity") {
+    isTournamentQuantityOk = numberRegex.test(input.value);
+    quantityErrorSpan.style.opacity = isTournamentQuantityOk ? 0 : 1;
+  }
+  let isOneTournamentChecked = TournamentArray.some(
+    (checkbox) => checkbox.checked
+  );
+  if (isOneTournamentChecked) {
+    isTournamentSelectOk = isOneTournamentChecked ? true : false;
+    locationsErrorSpan.style.opacity = isTournamentSelectOk ? 0 : 1;
+  }
+  if (input.id === "checkbox1") {
+    isCgvOk = cgv.checked;
+    cgvErrorSpan.style.opacity = isCgvOk ? 0 : 1;
+  }
+  infosEventsSpan.style.opacity = infosEvents.checked ? 1 : 0;
+};
+
+const onSubmitControl = () => {
+  firstNameErrorSpan.style.opacity = isFirstNameOk ? 0 : 1;
+  lastNameErrorSpan.style.opacity = isLastNameOk ? 0 : 1;
+  emailErrorSpan.style.opacity = isEmailOk ? 0 : 1;
+  birthdateErrorSpan.style.opacity = isBirthdateOk ? 0 : 1;
+  quantityErrorSpan.style.opacity = isTournamentQuantityOk ? 0 : 1;
+  locationsErrorSpan.style.opacity = isTournamentSelectOk ? 0 : 1;
+  cgvErrorSpan.style.opacity = isCgvOk ? 0 : 1;
+  if (
+    (isFormValid =
+      isFirstNameOk &&
+      isLastNameOk &&
+      isEmailOk &&
+      isBirthdateOk &&
+      isTournamentQuantityOk &&
+      isTournamentSelectOk &&
+      isCgvOk)
+  )
+    submitForm();
+};
+
+const submitForm = () => {
+  document.querySelector(".submit-ok").innerHTML = `
+  <p>Merci ! Votre réservation a été reçue.</p>
+  `;
+  document.querySelector("form").style.display = "none";
+  document.querySelector("form").reset();
+  isFirstNameOk = false;
+  isLastNameOk = false;
+  isEmailOk = false;
+  isBirthdateOk = false;
+  isTournamentQuantityOk = false;
+  isTournamentSelectOk = false;
+  isCgvOk = false;
+  let intervalId = setInterval(() => {
+    modalbg.style.display = "none";
+    document.querySelector(".submit-ok").innerHTML = `
+  `;
+    document.querySelector("form").style.display = "block";
+    clearInterval(intervalId);
+  }, 3000);
+};
+
+//Events
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
@@ -54,84 +134,6 @@ inputs.forEach((input) => {
   });
 });
 
-const inputControl = (input) => {
-  if (input.id === "firstName") {
-    firstNameOk = input.value.replaceAll(" ", "").length >= 3;
-    firstNameErrorSpan.style.opacity = firstNameOk ? 0 : 1;
-  }
-  if (input.id === "lastName") {
-    lastNameOk = input.value.replaceAll(" ", "").length >= 3;
-    lastNameErrorSpan.style.opacity = lastNameOk ? 0 : 1;
-  }
-  if (input.id === "email") {
-    emailOk = emailRegex.test(input.value);
-    emailErrorSpan.style.opacity = emailOk ? 0 : 1;
-  }
-  if (input.id === "birthdate") {
-    birthdateOk = birthdateRegex.test(input.value);
-    birthdateErrorSpan.style.opacity = birthdateOk ? 0 : 1;
-  }
-  if (input.id === "quantity") {
-    tournamentQuantityOk = numberRegex.test(input.value);
-    quantityErrorSpan.style.opacity = tournamentQuantityOk ? 0 : 1;
-  }
-  let isOneTournamentChecked = TournamentArray.some(
-    (checkbox) => checkbox.checked
-  );
-  if (isOneTournamentChecked) {
-    tournamentSelectOk = isOneTournamentChecked ? true : false;
-    locationsErrorSpan.style.opacity = tournamentSelectOk ? 0 : 1;
-  }
-  if (input.id === "checkbox1") {
-    cgvOk = cgv.checked;
-    cgvErrorSpan.style.opacity = cgvOk ? 0 : 1;
-  }
-  infosEventsSpan.style.opacity = infosEvents.checked ? 1 : 0;
-};
-
-const onSubmitControl = () => {
-  firstNameErrorSpan.style.opacity = firstNameOk ? 0 : 1;
-  lastNameErrorSpan.style.opacity = lastNameOk ? 0 : 1;
-  emailErrorSpan.style.opacity = emailOk ? 0 : 1;
-  birthdateErrorSpan.style.opacity = birthdateOk ? 0 : 1;
-  quantityErrorSpan.style.opacity = tournamentQuantityOk ? 0 : 1;
-  locationsErrorSpan.style.opacity = tournamentSelectOk ? 0 : 1;
-  cgvErrorSpan.style.opacity = cgvOk ? 0 : 1;
-  if (
-    (formValid =
-      firstNameOk &&
-      lastNameOk &&
-      emailOk &&
-      birthdateOk &&
-      tournamentQuantityOk &&
-      tournamentSelectOk &&
-      cgvOk)
-  )
-    submitForm();
-};
-
-const submitForm = () => {
-  document.querySelector(".submit-ok").innerHTML = `
-  <p>Merci ! Votre réservation a été reçue.</p>
-  `;
-  document.querySelector("form").style.display = "none";
-  document.querySelector("form").reset();
-  firstNameOk = false;
-  lastNameOk = false;
-  emailOk = false;
-  birthdateOk = false;
-  tournamentQuantityOk = false;
-  tournamentSelectOk = false;
-  cgvOk = false;
-  let intervalId = setInterval(() => {
-    modalbg.style.display = "none";
-    document.querySelector(".submit-ok").innerHTML = `
-  `;
-    document.querySelector("form").style.display = "block";
-    clearInterval(intervalId);
-  }, 3000);
-};
-//ISSUE 2 APPEL FONCTION SUBMIT
 document.querySelector("form").addEventListener("submit", (e) => {
   e.preventDefault();
   onSubmitControl();
